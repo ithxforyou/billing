a32764424f233c1504cee50a7fbcc546b0d616fd
Sarut Limpajeerawong| |github.tr4200812.github.com <179701381+tr4200812@users.noreply.github.com>
2025-05-04 17:23:27 +0700

    Initialize snyk-upgrade-3d5d5eee713970c506aef03f8dd5c42b

---
+++ src/providers/XmlContentProvider.ts
@@ -0,0 +1,35 @@
+import * as vscode from "vscode";
+
+/** Provides the contents of UDL documents extracted from XML files. */
+export class XmlContentProvider implements vscode.TextDocumentContentProvider {
+  /** A cache of UDL documents extracted from an XML file. */
+  private _udlDocsPerXmlFile: Map<string, { name: string; content: string[] }[]> = new Map();
+  private onDidChangeEvent: vscode.EventEmitter<vscode.Uri> = new vscode.EventEmitter<vscode.Uri>();
+
+  public provideTextDocumentContent(uri: vscode.Uri): string | undefined {
+    return this._udlDocsPerXmlFile
+      .get(uri.fragment)
+      ?.find((d) => d.name == uri.path)
+      ?.content.join("\n");
+  }
+
+  public get onDidChange(): vscode.Event<vscode.Uri> {
+    return this.onDidChangeEvent.event;
+  }
+
+  /**
+   * Add `udlDocs` extracted from XML file `uri` to the cache.
+   * Called by `previewXMLAsUDL()`.
+   */
+  public addUdlDocsForFile(uri: string, udlDocs: { name: string; content: string[] }[]): void {
+    this._udlDocsPerXmlFile.set(uri, udlDocs);
+  }
+
+  /**
+   * Remove UDL documents extracted from XML file `uri` from the cache.
+   * Called by `previewXMLAsUDL()`.
+   */
+  public removeUdlDocsForFile(uri: string): void {
+    this._udlDocsPerXmlFile.delete(uri);
+  }
+}
