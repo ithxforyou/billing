a4c7f598f3558225d5ebf228aa9914bea237baa1
Sarut Limpajeerawong| |github.tr4200812.github.com <179701381+tr4200812@users.noreply.github.com>
2025-05-07 01:36:45 +0700

    Ss
    ?
    
    Signed-off-by: Sarut Limpajeerawong| |github.tr4200812.github.com <179701381+tr4200812@users.noreply.github.com>

---
+++ src/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: ['src/**/*.*'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
